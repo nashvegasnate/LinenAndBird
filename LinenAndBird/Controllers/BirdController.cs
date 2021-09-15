3369ecf83712b9f224a6433d5661d64acac829e9
Nathan Keith <nashvegasnate@gmail.com>
2021-09-15 11:54:02 -0500

    added ADO.net commands on Bird repo & controller

--- LinenAndBird/Controllers/BirdController.cs
+++ LinenAndBird/Controllers/BirdController.cs
@@ -6,52 +6,81 @@ using System.Collections.Generic;
 
 namespace LinenAndBird.Controllers
 {
-    [Route("api/birds")]
-    [ApiController]
-    public class BirdController : ControllerBase
+  [Route("api/birds")]
+  [ApiController]    
+  public class BirdController : ControllerBase
+  {
+    BirdRepository _repo;
+    
+    public BirdController()
     {
-        BirdRepository _repo;
-
-        public BirdController()
-        {
-            _repo = new BirdRepository();
-        }
-
-        [HttpGet]
-        public IActionResult GetAllBirds()
-        {
-            //Returns an "Ok" response
-            return Ok(_repo.GetAll());
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetBirdById(Guid id)
-        {
-
-          var bird = _repo.GetById(id);
-
-          if (bird is null)
-          {
-            return NotFound($"No bird with the id {id} was found.");
-          }
-
-          return Ok(bird);
-        }
-
-        [HttpPost]
-        //IActionResult method - fancy way to return content & status codes together.
-        //It is an interface which implements the BadRequest and Ok response
-        public IActionResult AddBird(Bird newBird)
-        {
-            //Lets you know if you missed inputting a required property
-            if (string.IsNullOrEmpty(newBird.Name) || string.IsNullOrEmpty(newBird.Color))
-            {
-                return BadRequest("Name and Color are required fields");
-            }
-
-            _repo.Add(newBird);
-
-            return Created("api/birds/1", newBird);
-        }
+      _repo = new BirdRepository();
     }
+    
+    [HttpGet]
+    public IActionResult GetAllBirds()
+    {
+    //Returns an "Ok" response
+      return Ok(_repo.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetBirdById(Guid id)
+    {
+
+      var bird = _repo.GetById(id);
+
+       if (bird is null)
+       {
+        return NotFound($"No bird with the id {id} was found.");
+       }
+
+        return Ok(bird);
+    }
+
+    [HttpPost]
+    //IActionResult method - fancy way to return content & status codes together.
+    //It is an interface which implements the BadRequest and Ok response
+    public IActionResult AddBird(Bird newBird)
+    {
+    //Lets you know if you missed inputting a required property
+      if (string.IsNullOrEmpty(newBird.Name) || string.IsNullOrEmpty(newBird.Color))
+      {
+        return BadRequest("Name and Color are required fields");
+      }
+      
+      _repo.Add(newBird);
+
+      return Created($"api/birds/{newBird.Id}", newBird);
+    }
+
+    //api/birds/{id}
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBird(Guid id)
+    {
+      _repo.Remove(id);
+
+      return Ok();
+
+    }
+
+    //api/birds/{id}
+    [HttpPut("{id}")]
+    public IActionResult UpdateBird(Guid id, Bird bird)
+    {
+      var birdToUpdate = _repo.GetById(id);
+
+      if (birdToUpdate == null)
+      {
+        return NotFound($"Could not find bird with the id {id} for updating.");
+      }
+
+      var updatedBird = _repo.Update(id, bird);
+
+      return Ok(updatedBird);
+
+
+    }
+
+  }
 }
--- LinenAndBird/DataAccess/BirdRepository.cs
+++ LinenAndBird/DataAccess/BirdRepository.cs
@@ -7,99 +7,158 @@ using Microsoft.Data.SqlClient;
 
 namespace LinenAndBird.DataAccess
 {
-    public class BirdRepository
+  public class BirdRepository
+  {
+    const string _connectionString = "Server=localhost;Database=LinenAndBird;Trusted_Connection=True;";
+
+    internal IEnumerable<Bird> GetAll()
     {
-        static List<Bird> _birds = new List<Bird>
-        {
-            new Bird
-            {
-                Id = Guid.NewGuid(),
-                Name = "Jimmy",
-                Color = "Red",
-                Size = "Small",
-                Type = BirdType.Dead,
-                Accessories = new List<string> {"Beanie", "Gold wing tips"}
-            }
-        };
+      //connections are like the tunnel between our app and the database
+      using var connection = new SqlConnection(_connectionString);
+      //connections aren't open by default; we have to do that ourselves
+      connection.Open();
 
-        internal IEnumerable<Bird> GetAll()
-        {
-            //connections are like the tunnel between our app and the database
-            using var connection = new SqlConnection("Server=localhost;Database=LinenAndBird;Trusted_Connection=True;");
-            //connections aren't open by default; we have to do that ourselves
-            connection.Open();
+      //this is what tells sql what we want to do
+      var command = connection.CreateCommand();
+      command.CommandText = @"SELECT * 
+                              FROM Birds";
 
-            //this is what tells sql what we want to do
-            var command = connection.CreateCommand();
-            command.CommandText = @"SELECT * 
-                                    FROM Birds";
-            
-            //executeReader is for when we care about getting all the results of our query
-            var reader = command.ExecuteReader();
+      //executeReader is for when we care about getting all the results of our query
+      var reader = command.ExecuteReader();
 
-            var birds = new List<Bird>();      
+      var birds = new List<Bird>();
 
-            //data readers are weird; only get one row from the results at a time
-            while(reader.Read())
-            {
-                //Mapping data from the relational model to the object model
-                var bird = new Bird();
-                bird.Id = reader.GetGuid(0);
-                bird.Size = reader["Size"].ToString();
-                bird.Type = (BirdType)reader["Type"];
-                bird.Color = reader["Color"].ToString();
-                bird.Name = reader["Name"].ToString();
+      //data readers are weird; only get one row from the results at a time
+      while (reader.Read())
+      {
+        //Mapping data from the relational model to the object model
+        var bird = MapFromReader(reader);
 
-                //each bird goes in the list to return later
-                birds.Add(bird);
-            }
+        //each bird goes in the list to return later
+        birds.Add(bird);
+      }
 
 
-            return birds;
+      return birds;
 
-        }
+    }
 
-        internal void Add(Bird newBird)
-        {
-            newBird.Id = Guid.NewGuid();
-            _birds.Add(newBird);
-        }
+    internal Bird Update(Guid id, Bird bird)
+    {
+      var connection = new SqlConnection(_connectionString);
+      connection.Open();
 
-        internal Bird GetById(Guid birdId)
-        {
-          //connections are like the tunnel between our app and the database
-          using var connection = new SqlConnection("Server=localhost;Database=LinenAndBird;Trusted_Connection=True;");
-          //connections aren't open by default; we have to do that ourselves
-          connection.Open();
+      var cmd = connection.CreateCommand();
+      cmd.CommandText = @"UPDATE Birds
+                          SET Color = @color,
+                              Name = @name,
+                              Type = @type,
+                              Size = @size
+                          OUTPUT inserted.*
+                          WHERE id = @id";
+
+      //bird comes from the http request in the controller
+      cmd.Parameters.AddWithValue("Type", bird.Type);
+      cmd.Parameters.AddWithValue("Color", bird.Color);
+      cmd.Parameters.AddWithValue("Size", bird.Size);
+      cmd.Parameters.AddWithValue("Name", bird.Name);
+      cmd.Parameters.AddWithValue("id", id);
+
+      var reader = cmd.ExecuteReader();
+
+      if (reader.Read())
+      {
+        var updatedBird = MapFromReader(reader);
+        return updatedBird;
+      }
+
+      return null;
+
+    }
+
+    internal void Remove(Guid id)
+    {
+      using var connection = new SqlConnection(_connectionString);
+      connection.Open();
+
+      var cmd = connection.CreateCommand();
+      cmd.CommandText = @"Delete
+                          From Birds 
+                          Where Id = @id";
+
+      cmd.Parameters.AddWithValue("id", id);
+
+      cmd.ExecuteNonQuery();
+    }
+
+    internal void Add(Bird newBird)
+    {
+      using var connection = new SqlConnection(_connectionString);
+      connection.Open();
+
+      var cmd = connection.CreateCommand();
+      cmd.CommandText = @"insert into birds(Type,Color,Size,Name)
+                          output inserted.Id
+                          values (@Type, @Color,@Size,@Name)";
+
+      cmd.Parameters.AddWithValue("Type", newBird.Type);
+      cmd.Parameters.AddWithValue("Color", newBird.Color);
+      cmd.Parameters.AddWithValue("Size", newBird.Size);
+      cmd.Parameters.AddWithValue("Name", newBird.Name);
+
+      //execute the query, but don't care about the results; just the number of rows
+      //var numberOfRowsAffected = cmd.ExecuteNonQuery();
+
+      //execute the query and only get the id of the new row
+      var newId = (Guid)cmd.ExecuteScalar();
+
+      newBird.Id = newId;
+      //_birds.Add(newBird);
+    }
+
+    internal Bird GetById(Guid birdId)
+    {
+      //connections are like the tunnel between our app and the database
+      using var connection = new SqlConnection(_connectionString);
+      //connections aren't open by default; we have to do that ourselves
+      connection.Open();
 
 
-          var command = connection.CreateCommand();
-          command.CommandText = @"SELECT * 
+      var command = connection.CreateCommand();
+      command.CommandText = @"SELECT * 
                               FROM Birds
                               WHERE id = @id";
 
-          //IMPORTANT: parameterization prevents sql injection
-          command.Parameters.AddWithValue("id", birdId);
+      //IMPORTANT: parameterization prevents sql injection
+      command.Parameters.AddWithValue("id", birdId);
 
-          //executeReader is for when we care about getting all the results of our query
-          var reader = command.ExecuteReader();
+      //executeReader is for when we care about getting all the results of our query
+      var reader = command.ExecuteReader();
 
-          if (reader.Read())
-          {
-            var bird = new Bird();
-            bird.Id = reader.GetGuid(0);
-            bird.Size = reader["Size"].ToString();
-            bird.Type = (BirdType)reader["Type"];
-            bird.Color = reader["Color"].ToString();
-            bird.Name = reader["Name"].ToString();
+      if (reader.Read())
+      {
+        return MapFromReader(reader);
+      }
 
-            return bird;
-          }
+      return null;
 
-          return null;
-
-        }
-              //return _birds.FirstOrDefault(bird => bird.Id == birdId);
     }
+    //return _birds.FirstOrDefault(bird => bird.Id == birdId);
+
+
+    Bird MapFromReader(SqlDataReader reader)
+    {
+      var bird = new Bird();
+      bird.Id = reader.GetGuid(0);
+      bird.Size = reader["Size"].ToString();
+      bird.Type = (BirdType)reader["Type"];
+      bird.Color = reader["Color"].ToString();
+      bird.Name = reader["Name"].ToString();
+
+      return bird;
+    }
+
+
+  }
     
 }
